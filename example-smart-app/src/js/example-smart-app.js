7aa3b2f7cd2d363f7bdd8f202f24abebed90d4ca
CTMSS <chaitas.mee@gmail.com>
2025-10-16 18:20:52 +0700

    Improves patient data handling and safety checks
    Enhances the handling of patient data by adding safety checks for undefined values and ensuring proper array handling for names and observations.
    
    Introduces a `safeJoin` function for better management of potential array inputs. The changes also improve the robustness of blood pressure and cholesterol value retrieval by ensuring that empty results are handled gracefully.
    
    These modifications contribute to a more reliable and maintainable codebase.

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -7,58 +7,63 @@
       ret.reject();
     }
 
+    function safeJoin(x) {
+      return Array.isArray(x) ? x.join(' ') : (x || '');
+    }
+
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+          type: 'Observation',
+          query: {
+            code: {
+              $or: [
+                'http://loinc.org|8302-2',  // height
+                'http://loinc.org|8462-4',  // diastolic
+                'http://loinc.org|8480-6',  // systolic
+                'http://loinc.org|2085-9',  // HDL
+                'http://loinc.org|2089-1',  // LDL
+                'http://loinc.org|55284-4'  // BP panel
+              ]
+            }
+          }
+        });
 
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
+          var gender = patient.gender || '';
 
           var fname = '';
           var lname = '';
 
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+          if (patient.name && patient.name.length > 0) {
+            var n0 = patient.name[0];
+            fname = safeJoin(n0.given);   // given อาจเป็น array หรือ string
+            lname = safeJoin(n0.family);  // family เป็น string ใน R4 (รองรับ array ไว้เผื่อ)
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          var heightArr = byCodes('8302-2') || [];
+          var systolicbp = getBloodPressureValue(byCodes('55284-4') || [], '8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('55284-4') || [], '8462-4');
+          var hdlArr = byCodes('2085-9') || [];
+          var ldlArr = byCodes('2089-1') || [];
 
           var p = defaultPatient();
-          p.birthdate = patient.birthDate;
+          p.birthdate = patient.birthDate || '';
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
+          p.height = getQuantityValueAndUnit(heightArr[0]);
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
+          if (typeof systolicbp !== 'undefined')  p.systolicbp = systolicbp;
+          if (typeof diastolicbp !== 'undefined') p.diastolicbp = diastolicbp;
 
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.hdl = getQuantityValueAndUnit(hdlArr[0]);
+          p.ldl = getQuantityValueAndUnit(ldlArr[0]);
 
           ret.resolve(p);
         });
@@ -69,7 +74,6 @@
 
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
-
   };
 
   function defaultPatient(){
@@ -88,30 +92,29 @@
 
   function getBloodPressureValue(BPObservations, typeOfPressure) {
     var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
-        });
+    (BPObservations || []).forEach(function(observation){
+      var BP = (observation.component || []).find(function(component){
+        return (component.code && Array.isArray(component.code.coding)) &&
+               component.code.coding.find(function(coding) {
+                 return coding.code == typeOfPressure;
+               });
       });
-      if (BP) {
+      if (BP && BP.valueQuantity) {
         observation.valueQuantity = BP.valueQuantity;
         formattedBPObservations.push(observation);
       }
     });
-
     return getQuantityValueAndUnit(formattedBPObservations[0]);
   }
 
   function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
+    if (ob &&
+        ob.valueQuantity &&
+        typeof ob.valueQuantity.value !== 'undefined' &&
+        typeof ob.valueQuantity.unit  !== 'undefined') {
+      return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
     }
+    return undefined;
   }
 
   window.drawVisualization = function(p) {
@@ -127,5 +130,4 @@
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
   };
-
 })(window);
